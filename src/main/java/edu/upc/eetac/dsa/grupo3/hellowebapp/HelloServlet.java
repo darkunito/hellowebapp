6dbcb16e1515347920f6d8f4c43f1e87fc034a7f
darkunito <oriolcastanyo@gmail.com>
2015-04-25 22:56:21 +0200

    First commit

---
+++ src/main/java/edu/upc/eetac/dsa/grupo3/hellowebapp/HelloServlet.java
@@ -0,0 +1,42 @@
+package edu.upc.eetac.dsa.grupo3.hellowebapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	PrintWriter writer = new PrintWriter(response.getOutputStream());
+    	writer.println("Hello World!");
+    	writer.flush();
+    	writer.close();
+    	} 
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
